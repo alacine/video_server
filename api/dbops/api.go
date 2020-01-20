94ede261fdfd4bc251f0b00cfa626131a749b240
alacine <ryanruirr@gmail.com>
2020-01-20 14:47:54 +0800

    video api

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -3,7 +3,10 @@ package dbops
 import (
 	"database/sql"
 	"log"
+	"time"
 
+	"github.com/alacine/video_server/api/defs"
+	"github.com/alacine/video_server/api/utils"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -44,6 +47,56 @@ func DeleteUser(loginName string, pwd string) error {
 		return err
 	}
 	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	// create uuid
+	vid, err := utils.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	t := time.Now()
+	ctime := t.Format("Jan 02 2006, 15:04:05") //M D y, HH:MM:SS
+	stmtIns, err := dbConn.Prepare("INSERT INTO video_info (id, author_id, name, display_ctime) VALUES(?, ?, ?, ?)")
+	defer stmtIns.Close()
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmtIns.Exec(vid, aid, name, ctime)
+	if err != nil {
+		return nil, err
+	}
+	video := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: ctime}
+	return video, nil
+}
+
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	stmtOut, err := dbConn.Prepare("SELECT author_id, name, display_ctime FROM video_info WHERE id = ?")
+	var aid int
+	var dct string
+	var name string
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+	defer stmtOut.Close()
+	if err == sql.ErrNoRows {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	video := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: dct}
+	return video, nil
+}
+
+func DeleteVideoInfo(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_info WHERE id = ?")
+	defer stmtDel.Close()
+	if err != nil {
+		return err
+	}
+	_, err = stmtDel.Exec(vid)
 	if err != nil {
 		return err
 	}
--- api/dbops/api_test.go
+++ api/dbops/api_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+var tmpvid string
+
 func clearTables() {
 	dbConn.Exec("truncate users")
 	dbConn.Exec("truncate video_info")
@@ -21,8 +23,8 @@ func TestMain(m *testing.M) {
 func TestUserWorkFlow(t *testing.T) {
 	t.Run("Add", testAddUser)
 	t.Run("Get", testGetUser)
-	//t.Run("Del", testDeleteUser)
-	//t.Run("Reget", testRegetUser)
+	t.Run("Del", testDeleteUser)
+	t.Run("Reget", testRegetUser)
 }
 
 func testAddUser(t *testing.T) {
@@ -54,5 +56,45 @@ func testRegetUser(t *testing.T) {
 
 	if pwd != "" {
 		t.Errorf("Deleting user test failed")
+	}
+}
+
+func TestVideoWorkFlow(t *testing.T) {
+	clearTables()
+	t.Run("PrepareUser", testAddUser)
+	t.Run("AddVideo", testAddVideoInfo)
+	t.Run("GetVideo", testGetVideoInfo)
+	t.Run("DeleteVideo", testDeleteVideoInfo)
+	t.Run("RegetVideo", testRegetVideoInfo)
+}
+
+func testAddVideoInfo(t *testing.T) {
+	video, err := AddNewVideo(1, "test-video-first")
+	if err != nil {
+		t.Errorf("Error of AddVideoInfo: %v", err)
+	}
+	tmpvid = video.Id
+}
+
+func testGetVideoInfo(t *testing.T) {
+	//video, err := GetVideoInfo(tmpvid)
+	_, err := GetVideoInfo(tmpvid)
+	//if video.Name != "test-video-first" || err != nil {
+	if err != nil {
+		t.Errorf("Error of GetVideoInfo: %v", err)
+	}
+}
+
+func testDeleteVideoInfo(t *testing.T) {
+	err := DeleteVideoInfo(tmpvid)
+	if err != nil {
+		t.Errorf("Error of DeleteVideoInfo: %v", err)
+	}
+}
+
+func testRegetVideoInfo(t *testing.T) {
+	video, err := GetVideoInfo(tmpvid)
+	if video != nil && err != sql.ErrNoRows {
+		t.Errorf("Error of RegetVideoInfo: %v", err)
 	}
 }
